35f75dd839e06d5f0791ac85b4ed84e8201ea1b3
Raymond Chenon <raymond.chenon@gmail.com>
2018-09-20 21:39:25 +0200

    calculate the types

--- app/blockchain/services/BlockchainExplorerService.scala
+++ app/blockchain/services/BlockchainExplorerService.scala
@@ -7,8 +7,11 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class BlockchainExplorerService @Inject() (blockReader: BlockReader){
 
-  def calculateBalance(address: String) : BalanceResponse = {
-    BalanceResponse(calculateBalance4BlockType1(address))
+  def calculateBalance(address: String): Map[String,BalanceResponse] = {
+    Map(
+      "type1" -> BalanceResponse(calculateBalance4BlockType1(address)),
+      "type2" -> BalanceResponse(calculateBalance4BlockType2(address))
+    )
   }
 
   private def calculateBalance4BlockType1(address: String): Int = {
@@ -19,6 +22,20 @@ class BlockchainExplorerService @Inject() (blockReader: BlockReader){
         .foldLeft(0)(_ + _.amount)
       // if the address is the recipient, substract amount
       accBalance = accBalance + block.transactions.filter(t => t.sender == address).foldLeft(0)(_ - _.amount)
+    }
+
+    accBalance
+  }
+
+  private def calculateBalance4BlockType2(address: String): Int = {
+    var accBalance: Int = 0
+    for(block <- blockReader.parseTransactions2()){
+      // if the address is the recipient, add amount
+      accBalance = accBalance + block.transactions.filter(t => t.recipient == address)
+        .foldLeft(0)(_ + _.recipientBalanceChange)
+      // if the address is the recipient, substract amount
+      accBalance = accBalance + block.transactions.filter(t => t.sender == address)
+        .foldLeft(0)(_ + _.senderbalanceChange)
     }
 
     accBalance
