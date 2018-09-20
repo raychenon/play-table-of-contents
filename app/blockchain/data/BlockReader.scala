a86827701dda89904df593e1f66b0c3e7dab3a84
Raymond Chenon <raymond.chenon@gmail.com>
2018-09-21 00:52:35 +0200

    load the big file

--- app/blockchain/data/BlockReader.scala
+++ app/blockchain/data/BlockReader.scala
@@ -26,11 +26,11 @@ class BlockReader() {
   }
   
   def readType1(): String = {
-    read("public/assets/json/blockchain_type_1_with_4_blocks.json")
+    read("public/assets/json/blockchain_type_1_with_1000_blocks.json")
   }
 
   def readType2(): String = {
-    read("public/assets/json/blockchain_type_2_with_4_blocks.json")
+    read("public/assets/json/blockchain_type_2_with_1000_blocks.json")
   }
 
   private def read(source: String): String = {
