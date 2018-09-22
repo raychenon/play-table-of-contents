bc5926a7af7d8bfdb101ea27105e0c37c03aefbf
Raymond Chenon <raymond.chenon@gmail.com>
2018-09-22 18:08:31 +0200

    correct the math

--- app/blockchain/services/BlockchainExplorerService.scala
+++ app/blockchain/services/BlockchainExplorerService.scala
@@ -98,7 +98,9 @@ class BlockchainExplorerService @Inject() (blockReader: BlockReader){
       val transactionsContainingAddress = block.transactions.filter(t => t.recipient == address || t.sender == address)
       for(trx <- transactionsContainingAddress){
         val amount = trx.recipientBalanceChange
-        val fee = if(trx.senderbalanceChange == 0) 0 else -(trx.recipientBalanceChange + trx.senderbalanceChange)
+        // there is no fee for the genesis block ,
+        // senderbalanceChange is always negative, substract from it equals to add a positive number
+        val fee = if(trx.senderbalanceChange == 0) 0 else -(trx.senderbalanceChange + trx.recipientBalanceChange)
         val response = TransactionResponse(trx.sender,trx.recipient, amount,fee, block.date)
         listBuffer += response
       }
